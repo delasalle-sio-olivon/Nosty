6f88fdd79684902453df00c7d4e9f608b6d55f6f
Corentin356 <corentin.josse35@gmail.com>
2016-01-22 18:27:46 +0100

    Update cat.php

--- wp-content/themes/bbxdesert/cat.php
+++ wp-content/themes/bbxdesert/cat.php
@@ -3,7 +3,7 @@
       <div class="jumbotron jumbotronTop">
       	<div class="container logo">
       	<br>
-        <h1>Nosty, no stress!</h1>
+        <h1>Nosty, No Stress!</h1>
         <br>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
         <br>
