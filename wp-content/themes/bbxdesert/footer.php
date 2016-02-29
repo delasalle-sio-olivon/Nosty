1c6d33ba8a07d6f3df7ed86e37212e24fbd29ae7
delasalle-sio-olivon <delasalle.sio.olivon.a@gmail.com>
2016-02-29 11:16:41 +0100

    w

--- wp-content/themes/bbxdesert/footer.php
+++ wp-content/themes/bbxdesert/footer.php
@@ -2,7 +2,11 @@
 </div>
 </div><!-- /main-container -->
 <div id="footer">
+	<div class="foot1">
 	<p>© Nosty 2016</p><p class="colorWhite">icons8 . com</p>
+	</div>
+	<div class="foot1">
+	</div>
 </div>
 </div><!-- /main-site -->
 </body>
