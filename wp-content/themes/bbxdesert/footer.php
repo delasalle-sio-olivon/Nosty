4fa7f1b5dc5803731fdf74f5cf7633cb5e2f775e
delasalle-sio-olivon <delasalle.sio.olivon.a@gmail.com>
2016-03-01 15:22:37 +0100

    Footer

--- wp-content/themes/bbxdesert/footer.php
+++ wp-content/themes/bbxdesert/footer.php
@@ -3,9 +3,12 @@
 </div><!-- /main-container -->
 <div id="footer">
 	<div class="foot1">
-	<p>© Nosty 2016</p><p class="colorWhite">icons8 . com</p>
+	<p>Suivez nos aventures</p><p class="colorWhite">icons8 . com</p>
+	<a href="https://www.facebook.com/Nosty-987902074600378/?fref=ts"><i class="fa fa-facebook fa-3x iconInfo"></i></a>
+    <a href="https://twitter.com/Nosty_fr"><i class="fa fa-twitter fa-3x iconInfo"></i></a>
 	</div>
-	<div class="foot1">
+	<div class="foot2">
+	<div class="logoFoot"></div><div class="copyright">Nosty © 2016 – Tout droit réservés – Mention légales - Crédit</div>
 	</div>
 </div>
 </div><!-- /main-site -->
